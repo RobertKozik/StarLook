c07ac96e2f7f2b8d4ddb2158c9f21431198bacdf
Robert Kozik <rkozik98@gmail.com>
2021-03-30 20:42:30 +0200

    add basic fetch usability

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './style.css';
+import {getSwapiPeople} from '../../helpers/FetchWrapper'
+import { useEffect, useState } from 'react';
+import swapiPerson from '../../helpers/swapiPerson';
 
-function App() {
+
+const App = () => {
+  const [swapiResponse, setSwapiResponse] = useState([]);
+  const [page, setPage]= useState(1);
+
+  useEffect(()=> {
+    fetch("https://swapi.dev/api/people/?page="+page)
+    .then(res => res.json())
+    .then(json => {
+      const results = json.results;
+      console.log(results);
+      setSwapiResponse(prev => prev.concat(results));
+    })
+  },[page])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {swapiResponse.map((el: swapiPerson) => {
+        return (
+          <h1>{el.name}</h1>
+        )
+      })}
+      <button onClick={() => setPage(page+1)}>
+        click me
+      </button>
     </div>
   );
 }
