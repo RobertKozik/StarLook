23ac675685f0a72ac5a5aee6a5a079c89b19bebe
Robert Kozik <rkozik98@gmail.com>
2021-03-31 10:52:22 +0200

    fetch uri is now get from previous response

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -7,29 +7,38 @@ import Details from '../Details';
 
 const App = () => {
   const [swapiResponse, setSwapiResponse] = useState<swapiPerson[]>([]);
-  const [page, setPage]= useState<number>(1);
+  const [page, setPage]= useState<string|null>("https://swapi.dev/api/people/?page=1");
   const [currentlySelected, setCurrentlySelected] = useState<swapiPerson|null>(null)
 
-  const incrementPage = () => {
-    setPage(page+1);
-  }
 
-  useEffect(()=> {
-    fetch("https://swapi.dev/api/people/?page="+page)
+  const fetchData = () => {
+    if(page === null) return;
+
+    fetch(page)
     .then(res => res.json())
     .then(json => {
       const results = json.results;
-      console.log(results);
+      console.log(json);
       setSwapiResponse(prev => prev.concat(results));
+      json.next==null?setPage(null):setPage(changeToHttps(json.next));
     })
-  },[page])
+  }
+
+  useEffect(()=> {
+    fetchData();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[])
 
   return (
     <div className="main_container">
-    <NavBar People={swapiResponse} Click={incrementPage} SetSelected={setCurrentlySelected}/>
+    <NavBar People={swapiResponse} Click={fetchData} SetSelected={setCurrentlySelected}/>
     {currentlySelected != null && <Details Person={currentlySelected}/>}
     </div>
   );
+}
+
+const changeToHttps = (uri: string):string => {
+  return 'https'+uri.slice(4);
 }
 
 export default App;
